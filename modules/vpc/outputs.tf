558a181be4cc6a856f016b3b31fc431ee0d68a7b
Bogdan Zavoevanyy <bzavoevanyy@gmail.com>
2024-07-23 13:41:47 +0500

    refactoring

--- modules/vpc/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vpc_network_id" {
-  value = yandex_vpc_network.app-network.id
-}
-output "vpc_subnet_id" {
-  value = yandex_vpc_subnet.app-subnet.id
-}
--- modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_network_name" {
-  description = "VPC network name"
-}
-variable "folder_id" {
-  description = "Folder"
-}
-variable "vpc_subnet_name" {
-  description = "VPC subnet name"
-}
